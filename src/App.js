ecd163993deb96aaa778cf6d1f6036dd9312d85e
kelldintch <kelldintch@hotmail.com>
2018-06-30 21:15:40 -0500

    Uso de props para mandar datos de un HOC a un inferior

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import GroceryList from './GroceryList';
 import Header from './Header';
+
+const monday=[{id:1 ,name:"Bread"},{id:2,name:"Flowers"},{id:3,name:"Peanut butter"},{id:4,name:"Jelly"}]
+const tuesday=[{id:1 ,name:"Coffe"},{id:2,name:"Yougurt"},{id:3,name:"Cheese"},{id:4,name:"Wheat"}]
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Header/>
-        <GroceryList/>
+        <GroceryList list={monday}/>
       </React.Fragment>
     );
   }
--- src/GroceryList.js
+++ src/GroceryList.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-const data=[{id:1 ,name:"Bread"},{id:2,name:"Flowers"},{id:3,name:"Peanut butter"},{id:4,name:"Jelly"}]
+
 export default class GroceryList extends Component {
   render() {
     return (
       <React.Fragment>          
           <ol>
-              {data.map((item)=>{
+              {this.props.list.map((item)=>{
                   return <li key={item.id}>{item.name}</li>
               })}
           </ol>
