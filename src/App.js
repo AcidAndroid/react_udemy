c330ac156e6cbf5ae36400c8835ebbfa1596cbdb
kelldintch <kelldintch@hotmail.com>
2018-06-30 23:57:20 -0500

    Usando state para eventos

--- src/App.js
+++ src/App.js
@@ -3,22 +3,39 @@ import './App.css';
 import GroceryList from './GroceryList';
 import Header from './Header';
 
-const monday=[{id:1 ,name:"Bread"},{id:2,name:"Flowers"},{id:3,name:"Peanut butter"},{id:4,name:"Jelly"}]
-const tuesday=[{id:1 ,name:"Coffe"},{id:2,name:"Yougurt"},{id:3,name:"Cheese"},{id:4,name:"Wheat"}]
 
 class App extends Component {
+  
+  state ={
+    count:0
+  }
+
+  
+  incrementCount =()=> {
+    this.setState((prevState)=>{
+      return{
+        count: prevState.count +1
+      }
+    })
+  }
+
+  decrementCount =()=> {
+    this.setState((prevState)=>{
+      return{
+        count: prevState.count -1
+      }
+    })
+  }
+  
+  
   render() {
     return (
       <React.Fragment>
-        <Header/>
-        <GroceryList />
-
-        <Header day={'Monday'}/>
-        <GroceryList list={monday}/>
-
-        <Header day={'Tuesday'}/>
-        <GroceryList list={tuesday}/>
-        
+        <h1>
+          {this.state.count}
+        </h1>
+       <button onClick={this.incrementCount} >+</button>
+       <button onClick={this.decrementCount} >-</button>
       </React.Fragment>
     );
   }
