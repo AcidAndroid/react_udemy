85f3f7da7f18a7ccaefa427078c2fb9669d7c9c2
kelldintch <kelldintch@hotmail.com>
2018-06-30 20:59:33 -0500

    Uso de una fuente de datos para generar datos dinamicos

--- src/GroceryList.js
+++ src/GroceryList.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 
+const data=[{id:1 ,name:"Bread"},{id:2,name:"Flowers"},{id:3,name:"Peanut butter"},{id:4,name:"Jelly"}]
 export default class GroceryList extends Component {
   render() {
     return (
       <React.Fragment>          
           <ol>
-              <li>Bread</li>
-              <li>Flowers</li>
-              <li>Peanut butter</li>
+              {data.map((item)=>{
+                  return <li key={item.id}>{item.name}</li>
+              })}
           </ol>
       </React.Fragment>
     )
