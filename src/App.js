5183d9d53a686a1319b6e6ed92f3eeec8bba0194
kelldintch <kelldintch@hotmail.com>
2018-07-02 22:36:55 -0500

    Forms; uso de ref , onchange y guardar los cambios en el state

--- src/App.js
+++ src/App.js
@@ -7,10 +7,23 @@ import Square from './Square';
 
 class App extends Component {
 
+  state={
+    input:'Puedes escribir aqui'
+  }
+
+  handleOnchange = (e)=>{
+    console.log(e.target)
+    this.setState({
+      input:e.target.value
+    })
+  }
   render(){
     return(
       <React.Fragment>
-        
+        <input defaultValue='Pueden escribir'  type="text" ref={(input)=>{this.inputField=input}}></input>
+        <input value='Soy readony'  type="text" ref={(input)=>{this.inputField=input}}></input>
+        <input value={this.state.input}  type="text" onChange={this.handleOnchange}></input>
+        <button onClick={()=>{console.log(this.inputField.value)}} ></button>
       </React.Fragment>
     )
   }
